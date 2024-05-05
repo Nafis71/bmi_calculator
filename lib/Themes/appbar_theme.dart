9d1285b572fcdf907d40dafd1fd30e4e89002481
Nafis71 <nhtonmoy2@gmail.com>
2024-05-05 20:43:26 +0600

    Icons and App name changed

--- lib/Themes/appbar_theme.dart
+++ lib/Themes/appbar_theme.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class AppbarTheme{
   static getAppbarTheme() => const AppBarTheme(
     backgroundColor: Colors.white,
+    scrolledUnderElevation: 0.0,
     titleTextStyle: TextStyle(
       fontWeight: FontWeight.w700,
       fontSize: 20,
